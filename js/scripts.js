1e79c09bbfff35340ab78ff8d3de9934bfc1a81e
Nick Tse <nicholashtse@gmail.com>
2022-05-08 03:14:23 -0700

    Add id to display total price for entire order to the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-// create function to calculate price based on toppings added: Pepperoni, Italian Sausage, Grilled Chicken, Onions, Roasted Red Peppers, Mushrooms
-// create function to calculate price based on size selected: small 10", medium 12", large 14", X-large 16"
-// allow user can order multiple pizzas (probably need something like addressbook). Would you like to order more? If yes, new form. If no, display total $$ for the order and button to place order
-
 function CustomerOrder() {
   this.items = [];
   this.totalOrderPrice;
@@ -89,6 +85,7 @@ $(document).ready(function() {
     newOrder.price = newOrder.priceBySize() + newOrder.priceByToppings();
     customerOrder.addToOrder(newOrder);
     displayOrderDetails(customerOrder);
+    $("#orderTotal").html(customerOrder.calculateTotalPrice());
     $("#output").show();
   });
 });
